7c65a1429d87a065d28907bcce1cf6d92653adba
impratik-code <pratikahire5456@gmail.com>
2023-02-17 01:09:13 -0500

    first commit

---
+++ TestApplicationGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApplicationGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
